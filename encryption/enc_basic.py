4c8ab36bc44637c46a13a1fd8ecb2ef0735db78f
Balazs Bucsay <xfltreat@rycon.hu>
2018-05-06 21:43:05 +0100

    removing comment

--- encryption/enc_basic.py
+++ encryption/enc_basic.py
@@ -33,7 +33,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
-#from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 import common
