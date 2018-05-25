6822615b559d7e0ea2f3f1743e2578372180fc48
Balazs Bucsay <xfltreat@rycon.hu>
2018-05-25 18:23:01 +0100

    is_hostname changed,real life doesn't follow rfc

--- common.py
+++ common.py
@@ -317,7 +317,7 @@ def is_control_channel(control_character):
 
 # Shamelessly stolen regular expressions, any of them could be wrong
 def is_hostname(s):
-	return bool(re.match("^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$", s))
+	return bool(re.match("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$", s))
 
 def is_ipv4(s):
 	return bool(re.match("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", s))
